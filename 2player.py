77e648b91ceafccad7f5814d373a895351f4dd94
Justin <justinprivitera@gmail.com>
2019-03-14 09:55:47 -0700

    removed debugging code

--- 2player.py
+++ 2player.py
@@ -35,7 +35,7 @@ def play():
 
 	# display results, start new game
 	print(str_board(board), end = '')
-	
+
 def check_win(board): # return 0 if O's win, 1 if X's win, and -1 if it is incomplete and 2 if it is a draw
 # abstract this method into more smaller methods
 	sum_diag = 0
@@ -43,10 +43,8 @@ def check_win(board): # return 0 if O's win, 1 if X's win, and -1 if it is incom
 	i = 0
 	while i < BOARD_LENGTH:
 		if sum(board[i]) == BOARD_LENGTH:
-			print("flag 1")
 			return 1
 		elif sum(board[i]) == -BOARD_LENGTH:
-			print("flag 2")
 			return 0
 		
 		j = 0
@@ -54,31 +52,24 @@ def check_win(board): # return 0 if O's win, 1 if X's win, and -1 if it is incom
 			sum_col += board[j][i]
 			j += 1
 		if sum_col == BOARD_LENGTH:
-			print("flag 3")
 			return 1
 		elif sum_col == -BOARD_LENGTH:
-			print("flag 4")
 			return 0
 		
 		sum_diag += board[i][i]
 		i += 1
 	
 	if sum_diag == BOARD_LENGTH:
-		print("flag 5")
 		return 1
 	elif sum_diag == -BOARD_LENGTH:
-		print("flag 6")
 		return 0
 	sum_diag = board[0][2] + board[1][1] + board[2][0]
 	if sum_diag == BOARD_LENGTH:
-		print("flag 7")
 		return 1
 	elif sum_diag == -BOARD_LENGTH:
-		print("flag 8")
 		return 0
 
 	if check_draw(board) == 1:
-		print("flag 9")
 		return 2
 
 	return -1
